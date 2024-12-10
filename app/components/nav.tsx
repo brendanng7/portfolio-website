fa0cac604892dd78dacb8a0d3c02a57d174b674b
brendanng7 <brendanngjj@gmail.com>
2024-12-11 00:29:13 +0800

    Add socials in navbar

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,4 +1,5 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import Image from 'next/image';
 
 const navItems = {
   '/': {
@@ -7,17 +8,30 @@ const navItems = {
   '/blog': {
     name: 'blog',
   },
-  'https://vercel.com/templates/next.js/portfolio-starter-kit': {
-    name: 'deploy',
+  '/gallery': {
+    name: 'gallery',
   },
-}
+};
+
+const socials = {
+  github: {
+    name: 'github',
+    url: 'github.com/brendanng7',
+    svg: '/socials/github.svg',
+  },
+  linkedin: {
+    name: 'linkedin',
+    url: 'linkedin.com/in/brendanng7',
+    svg: '/socials/linkedin.svg',
+  },
+};
 
 export function Navbar() {
   return (
     <aside className="-ml-[8px] mb-16 tracking-tight">
       <div className="lg:sticky lg:top-20">
         <nav
-          className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+          className="flex flex-row items-start justify-between relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
           id="nav"
         >
           <div className="flex flex-row space-x-0 pr-10">
@@ -30,11 +44,32 @@ export function Navbar() {
                 >
                   {name}
                 </Link>
-              )
+              );
+            })}
+          </div>
+          <div className="flex flex-row space-x-0 pr-10">
+            {Object.entries(socials).map(([social, { name, url, svg }]) => {
+              return (
+                <>
+                  <a
+                    key={social}
+                    href={`https://${url}`}
+                    className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
+                  >
+                    <Image
+                      src={svg}
+                      alt={name}
+                      width={24}
+                      height={24}
+                      className=""
+                    />
+                  </a>
+                </>
+              );
             })}
           </div>
         </nav>
       </div>
     </aside>
-  )
+  );
 }
